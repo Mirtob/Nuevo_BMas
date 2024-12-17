2cdb1fcdeb7c54bf7dc7d3ddf7f39626498eaff8
gtobar <113481289+Mirtob@users.noreply.github.com>
2024-12-17 09:42:21 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NuevoBMas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Home.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using NuevoBmas;
+using NuevoBmas_Datos;
+
+namespace NuevoBmas
+{
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Navigate_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Cajas());
+        }
+    }
+}
